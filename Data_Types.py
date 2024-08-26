44d5bdb47dc6fad7741925931af317ef10712f41
jchieroosborne <89059659+jchieroosborne@users.noreply.github.com>
2024-08-26 14:55:42 -0500

    Add files via upload

---
+++ Data_Types.py
@@ -0,0 +1,23 @@
+var = 'Jayson'
+age = 21
+name = "Jayson"
+
+print('My name is',var)
+print("My age is",age)
+
+x = 10
+print('The literal is',x)
+
+y = 15
+print('integer:',y)
+
+z = 25
+print("Float:", z)
+
+name = 'Jayson'
+print ('String:', name)
+
+is_active = True
+print('Boolean:', is_active)
+
+print(x, '+', y, "=", z)
