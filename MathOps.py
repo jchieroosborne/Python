7a007c6e597ba677604ce3e2bd0647afaeccf3a2
jchieroosborne <89059659+jchieroosborne@users.noreply.github.com>
2024-08-26 15:33:41 -0500

    Add files via upload

---
+++ MathOps.py
@@ -0,0 +1,13 @@
+# Multiplication for Area
+L = 4
+W = 8
+product = L * W
+print('\nCalculation for Area:\nThe Area of a Rectangle with a Length of 4 inches and Width of 8 inches is', product,'inches because Length x Width = Area')
+
+
+# Multiplication for Volume
+L = 4
+W = 8
+H = 2
+product = L * W * H 
+print('\nCalculation for Volume:\nThe Volume of a Rectangle with a Length of 4 inches, a Width of 8 inches, and a Height of 2 inches is', product,'inches because Length x Width x Height = Volume\n')
