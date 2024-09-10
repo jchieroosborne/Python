40d2eb87c219f21f6277b111686d607a36e0ed7a
jchieroosborne <89059659+jchieroosborne@users.noreply.github.com>
2024-09-09 21:56:38 -0500

    Add files via upload

---
+++ divisible_by_7.py
@@ -0,0 +1,10 @@
+
+#Iterate through the range of numbers from 1 to 300
+
+for number in range(1, 301):
+        
+#Check if the number is divisible by 7
+    if number % 7 == 0:
+
+#Print the number if it is divisible by 7
+        print(number)
