649052cfcb20d8ccf91e1697bacfed6ea705584d
Jayson Chiero-Osborne <89059659+jchieroosborne@users.noreply.github.com>
2024-10-09 08:18:23 -0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,7 @@
+from math_operations import calculator
+from math_operations import geometry
+result = calculator.add(5, 3)
+print("Addition result:", result)
+
+result = calculator.subtract(10, 4)
+print("Subtraction result:", result)
